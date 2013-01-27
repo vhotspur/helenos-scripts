2868abad041d73917378854a6dcdd6389604f57d
Vojtech Horky <vojtech.horky@gmail.com>
2013-01-27 22:27:34 +0100

    Update float.h generator

--- generate-float-h.c
+++ generate-float-h.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <float.h>
 
+#define GROUP(name) \
+	printf("\n/* %s */\n", name);
+
+#define PRINT_(name, value, specifier) \
+	printf("#define %s %.200Lg%s\n", name, (long double) value, specifier)
+
 #define PRINT(constant) \
-	printf("#define %s (%Lg)\n", #constant, (long double) constant)
+	PRINT_(#constant, constant, "")
+
+#define PRINTL(constant) \
+	PRINT_(#constant, constant, "L")
 
 
 int main(int argc, char *argv[]) {
@@ -10,19 +19,36 @@ int main(int argc, char *argv[]) {
 	printf("#ifndef _FLOAT_H_INCLUDE_GUARD\n");
 	printf("#define _FLOAT_H_INCLUDE_GUARD\n");
 	printf("\n\n\n");
-	PRINT(DBL_MIN);
-	PRINT(DBL_MAX);
-	PRINT(DBL_EPSILON);
-	PRINT(LDBL_MIN);
-	PRINT(LDBL_MAX);
-	PRINT(LDBL_EPSILON);
+
+	GROUP("float limits")
 	PRINT(FLT_MIN);
 	PRINT(FLT_MAX);
+
+	GROUP("double limits")
+	PRINT(DBL_MIN);
+	PRINT(DBL_MAX);
+
+	GROUP("long double limits")
+	PRINTL(LDBL_MIN);
+	PRINTL(LDBL_MAX);
+
+	GROUP("epsilons")
 	PRINT(FLT_EPSILON);
+	PRINT(DBL_EPSILON);
+	PRINTL(LDBL_EPSILON);
+
+	GROUP("float radix")
 	PRINT(FLT_RADIX);
-	PRINT(LDBL_MANT_DIG);
-	PRINT(DBL_MANT_DIG);
+
+	GROUP("mantisa")
 	PRINT(FLT_MANT_DIG);
+	PRINT(DBL_MANT_DIG);
+	PRINT(LDBL_MANT_DIG);
+
+	GROUP("exponents")
+	PRINT(DBL_MIN_EXP);
+	PRINT(DBL_MAX_EXP);
+
 	printf("\n\n\n");
 	printf("#endif\n");
 	return 0;
